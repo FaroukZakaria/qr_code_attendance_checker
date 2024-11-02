eb336225a61131e5ec13d98656732b3eeedfcec7
Farouk Zakaria <gameswithoutlose97@gmail.com>
2024-11-03 00:45:24 +0200

    database related credentials and tables

---
+++ project/db.py
@@ -0,0 +1,9 @@
+import os
+from pymongo import MongoClient
+
+mongo_user = os.getenv('MONGODB_USERNAME')
+mongo_password = os.getenv('MONGODB_PASSWORD')
+mongo_uri = f"mongodb+srv://{mongo_user}:{mongo_password}@firstserver.z0ezt.mongodb.net/api-project?retryWrites=true&w=majority&appName=FirstServer"
+client = MongoClient(mongo_uri)
+db = client['qr-attendance']
+users_collection = db['users']
