348741c0ccda5b92cb4d0eb5c4103cf90df7ef4e
Farouk Zakaria <gameswithoutlose97@gmail.com>
2024-11-04 02:16:18 +0200

    removed debugger

--- project/controllers/user_controller.py
+++ project/controllers/user_controller.py
@@ -4,5 +4,4 @@ def create_user(user):
     users_collection.insert_one(user.dict())
 
 def get_user_by_section_and_number(section: int, number:int):
-    print(users_collection.find_one({'section': section, 'number': number}))
     return users_collection.find_one({'section': section, 'number': number})
