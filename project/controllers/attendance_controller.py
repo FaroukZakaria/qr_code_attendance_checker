a041f520f247c366072356ff9c120d5335d056ad
Farouk Zakaria <gameswithoutlose97@gmail.com>
2024-11-04 02:16:08 +0200

    implemented attendance record in controller file

--- project/controllers/attendance_controller.py
+++ project/controllers/attendance_controller.py
@@ -1,3 +1,18 @@
-def add_attendance(name, section, number):
-    # Add attendance to the database
-    pass
+from project.models.users_model import User
+from project.models.attendance_model import Attendance
+from project.db import attendance_collection
+from datetime import datetime
+
+def add_attendance(user: User, teacher: User, topic: str | None=None) -> None:
+    name = user['name']
+    section = user['section']
+    number = user['number']
+
+    attendance = Attendance(
+        student=User(name=name, section=section, number=number),
+        teacher=teacher,
+        date=datetime.now(),
+        topic=topic
+    )
+
+    attendance_collection.insert_one(attendance.dict())
