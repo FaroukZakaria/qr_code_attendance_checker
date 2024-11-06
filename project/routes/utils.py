027b6bc5c6d8c9d38ee4961c1dc74992afee5a71
Farouk Zakaria <gameswithoutlose97@gmail.com>
2024-11-06 22:25:25 +0200

    added a logic to check if cookies include tokens (user is logged in) then it would redirect to the dashboard, if not then stay at index

--- project/routes/utils.py
+++ project/routes/utils.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
+from project.controllers.token_controller import verify_token
+
 
 app = Blueprint('utils', __name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    token = request.cookies.get('access_token')
+    if not token or not verify_token(token):
+        return render_template('index.html')
+    return redirect('record-attendance')
