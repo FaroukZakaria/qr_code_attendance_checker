58596c8ec95a6d66dfeeb88edaca1326b435ca55
Farouk Zakaria <gameswithoutlose97@gmail.com>
2024-11-07 00:35:46 +0200

    get user by token controller

--- project/controllers/user_controller.py
+++ project/controllers/user_controller.py
@@ -1,5 +1,6 @@
 from project.db import users_collection
 from project.models.users_model import User, AdminUser
+from project.routes.utils import verify_token
 
 def create_user(user: User):
     users_collection.insert_one(user.dict())
@@ -12,3 +13,6 @@ def get_user_by_section_and_number(section: int, number:int):
 
 def get_admin(email: str):
     return users_collection.find_one({'email': email})
+
+def get_user_by_token(token: str):
+    users_collection.find_one({'_id': verify_token(token)['sub']})
