59e6625df14c8d5bb276cacb5d913f5ca916bcfe
Farouk Zakaria <gameswithoutlose97@gmail.com>
2024-11-07 00:35:25 +0200

    host change

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from project import app
 
 if __name__ == '__main__':
     print(f"Starting server: http://localhost:8080")
-    app.run(host='localhost', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
