f68ea4271a103b303685ef38912f7f7fa5fce70c
Farouk Zakaria <gameswithoutlose97@gmail.com>
2024-11-06 22:11:12 +0200

    added controllers for admin

--- project/controllers/user_controller.py
+++ project/controllers/user_controller.py
@@ -1,7 +1,14 @@
 from project.db import users_collection
+from project.models.users_model import User, AdminUser
 
-def create_user(user):
+def create_user(user: User):
     users_collection.insert_one(user.dict())
+
+def create_admin(admin: AdminUser):
+    users_collection.insert_one(admin.dict())
 
 def get_user_by_section_and_number(section: int, number:int):
     return users_collection.find_one({'section': section, 'number': number})
+
+def get_admin(email: str):
+    return users_collection.find_one({'email': email})
